89db8f5fbb483b3c27b75c8cf4326dc3bacd3a9a
Haisia <cwnsgur13579@gmail.com>
2025-04-19 15:41:40 +0900

    신규: 커스텀 색상 `myblack` 작성

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './src/app/**/*.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    colors: {
+      myblack: "#1d1d1d",
+    },
+    extend: {
+      colors: {
+        myblack: "#1d1d1d",
+      },
+    },
+  },
+  plugins: [],
+};
