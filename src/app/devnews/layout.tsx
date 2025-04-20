5623eae0df1851f010d445c5ec6e1df463ef7e9f
Haisia <cwnsgur13579@gmail.com>
2025-04-20 18:07:21 +0900

    신규: Dev News 페이지 NavBar 구현

--- src/app/devnews/layout.tsx
+++ src/app/devnews/layout.tsx
@@ -1,4 +1,5 @@
 import React, {ReactNode} from "react";
+import Link from "next/link";
 
 const Layout = async (
   {
@@ -19,10 +20,29 @@ const Layout = async (
 
 const PostNavBar = () => {
   return (
-    <div className="w-[120px] border-r border-gray-500/30">
-      <div className="p-4">postNavBar</div>
-      <div className="p-4">test</div>
+    <div className="w-[400px] border-r border-gray-500/30 px-4 py-6 [&>*]:px-2 [&>div]:py-4">
+      <DropdownNavBar/>
+      <ol
+        className="list-decimal list-outside space-y-4 py-4 marker:text-lg marker:font-bold [&>li]:text-gray-400 mx-5 [&>li]:text-gray-400">
+        <li><Link className={"hover:text-mypurple-100"} href={"/devnews/3"}>2025년 개발 트렌드</Link></li>
+        <li className={"hover:text-mypurple-100"}>웹 개발을 변화시키는 AI: 코딩 보조 도구의 미래</li>
+        <li className={"hover:text-mypurple-100"}>ES2024 초안 공개: JavaScript의 다음 단계는?</li>
+        <li className={"hover:text-mypurple-100"}>Next.js 16 발표: 서버 렌더링과 CSR의 경계 허물기</li>
+      </ol>
     </div>
   );
 };
 export default Layout;
+
+const DropdownNavBar = () => {
+  return (
+    <>
+      <select className={"text-white [&>option]:bg-myblack w-full border-gray-500/30 border-2 rounded-lg py-2"}>
+        <option>2025년 4월</option>
+        <option>2025년 3월</option>
+        <option>2025년 2월</option>
+        <option>2025년 1월</option>
+      </select>
+    </>
+  );
+}
