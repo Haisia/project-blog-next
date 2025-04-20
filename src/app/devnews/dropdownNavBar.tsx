7d4aab5e1b870af372ee0ae24170d1bba9549bee
Haisia <cwnsgur13579@gmail.com>
2025-04-20 23:16:11 +0900

    수정: PostNavBar 동적 구현

--- src/app/devnews/dropdownNavBar.tsx
+++ src/app/devnews/dropdownNavBar.tsx
@@ -1,20 +1,20 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
 export interface YearMonth {
   year: string;
   month: string;
 }
 
-const DropdownNavBar = () => {
+const DropdownNavBar = ({onChangeHandler}:{onChangeHandler:(value:string)=>void}) => {
   const [yearMonths, setYearMonths] = useState<YearMonth[]>([]);
 
   useEffect(() => {
     const fetchYearMonths = async () => {
       const response = await fetch("http://localhost:8080/api/blog/devnews/all/year-month");
       const data = await response.json();
-      const { allUniqueYearMonth }: { allUniqueYearMonth: YearMonth[] } = data;
+      const {allUniqueYearMonth}: { allUniqueYearMonth: YearMonth[] } = data;
 
       setYearMonths(allUniqueYearMonth);
     };
@@ -23,9 +23,15 @@ const DropdownNavBar = () => {
   }, []);
 
   return (
-    <select className="text-white [&>option]:bg-myblack w-full border-gray-500/30 border-2 rounded-lg py-2">
+    <select
+      className="text-white [&>option]:bg-myblack w-full border-gray-500/30 border-2 rounded-lg py-2"
+      onChange={(e) => {onChangeHandler(e.target.value);}}
+    >
       {yearMonths.map((yearMonth) => (
-        <option key={yearMonth.year + yearMonth.month} value={yearMonth.year + yearMonth.month}>
+        <option
+          key={yearMonth.year + yearMonth.month}
+          value={yearMonth.year + yearMonth.month}
+        >
           {yearMonth.year}년 {yearMonth.month}월
         </option>
       ))}
--- src/app/devnews/postNavBar.tsx
+++ src/app/devnews/postNavBar.tsx
@@ -13,23 +13,35 @@ export interface Article {
 const PostNavBar = () => {
   const [articles, setArticles] = useState<Article[]>([]);
   const [activeId, setActiveId] = useState<string | null>(null);
+  const [selectedYearMonth, setSelectedYearMonth] = useState<string | null>(null);
+
 
   useEffect(() => {
     const fetchArticles = async () => {
-      const response = await fetch("http://localhost:8080/api/blog/devnews/all/latest");
+      let response;
+      if (!selectedYearMonth) {
+        response = await fetch("http://localhost:8080/api/blog/devnews/all/latest");
+      } else {
+        response = await fetch(`http://localhost:8080/api/blog/devnews/all/by/year-month/${selectedYearMonth}`);
+      }
       const data = await response.json();
-      const { articles }: { articles: Article[] } = data;
+      const articlesData: Article[] = Array.isArray(data) ? data : [];
 
-      articles.sort((a, b) => Number(b.id) - Number(a.id));
-      setArticles(articles);
+      articlesData.sort((a, b) => Number(b.id) - Number(a.id));
+      setArticles(articlesData);
     };
 
     fetchArticles();
-  }, []);
+
+    console.log(articles)
+
+  }, [selectedYearMonth]);
+
+  const handleOnChangeSelectedYearMonth = (value:string) => setSelectedYearMonth(value);
 
   return (
     <div className="w-[400px] border-r border-gray-500/30 px-4 py-6 [&>*]:px-2 [&>div]:py-4">
-      <DropdownNavBar />
+      <DropdownNavBar onChangeHandler={handleOnChangeSelectedYearMonth} />
       <ol className="list-decimal list-outside space-y-4 py-4 marker:text-lg marker:font-bold [&>li]:text-gray-400 mx-5 [&>li]:text-gray-400">
         {articles.map((article) => (
           <li key={article.id}>
