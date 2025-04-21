4927c3cccd33b935c45a5ac20b81cbddeccf6f0b
최준혁 <cwnsgur13579@5finger.co.kr>
2025-04-21 14:16:09 +0900

    수정: 마크다운 랜더링 플러그인추가

--- src/app/devnews/[id]/page.tsx
+++ src/app/devnews/[id]/page.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import {MDXRemote} from "next-mdx-remote/rsc";
-import {fetchDevNews} from "@/api/fetchDevNews";
+// page.tsx
+import { compileMDX } from 'next-mdx-remote/rsc'
+import remarkGfm from 'remark-gfm'
+import { fetchDevNews } from "@/api/fetchDevNews"
 
-const Page = async (
-  {
-    params,
-  }: Readonly<{
-    params: Promise<{id: number}>,
-  }>) => {
+const Page = async ({ params }: Readonly<{ params: Promise<{ id: number }> }>) => {
+  const { id } = await params
+  const response = await fetchDevNews(id)
+  const devNews = response.blogDevNewses[0]
 
-  const { id } = await params;
-  const response = await fetchDevNews(id);
-  const devNews = response.blogDevNewses[0];
+  const { content, frontmatter } = await compileMDX({
+    source: devNews.contentData.content,
+    options: {
+      mdxOptions: {
+        remarkPlugins: [remarkGfm],
+      },
+    },
+  })
 
   return (
     <>
       <h1 className="text-[2.5rem] font-bold my-5">{devNews.contentData.title}</h1>
       <article className="markdown max-w-none">
-        <MDXRemote source={devNews.contentData.content} />
+        {content}
       </article>
     </>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
