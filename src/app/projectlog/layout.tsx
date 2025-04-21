c91dde89d76787e44387b2644154f03eda37200c
최준혁 <cwnsgur13579@5finger.co.kr>
2025-04-21 19:42:48 +0900

    신규: #11 Project Log 레이아웃 잡기

--- src/app/projectlog/layout.tsx
+++ src/app/projectlog/layout.tsx
@@ -1,10 +1,10 @@
 import React, { ReactNode } from "react";
+import ProjectLogNevBar from "@/app/projectlog/projectLogNevBar";
 
 const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <div className="flex flex-1 items-stretch">
-      {/*<PostNavBar />*/}
-      <div>fsdffs</div>
+      <ProjectLogNevBar/>
       <div className="flex-1">
         {children}
       </div>
