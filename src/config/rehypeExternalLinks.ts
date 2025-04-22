aa3991237c0f78f5550ac84e554ccd55ee81ecf0
Haisia <cwnsgur13579@gmail.com>
2025-04-22 22:52:49 +0900

    수정: 마크다운 포스트 랜더링 수정

--- src/config/rehypeExternalLinks.ts
+++ src/config/rehypeExternalLinks.ts
@@ -1,7 +1,8 @@
 import { visit } from 'unist-util-visit';
+import type { Root } from 'hast';
 
 export default function rehypeExternalLinks() {
-  return (tree: any) => {
+  return (tree: Root) => {
     visit(tree, 'element', (node) => {
       if (node.tagName === 'a' && node.properties && typeof node.properties.href === 'string') {
         node.properties.target = '_blank';
