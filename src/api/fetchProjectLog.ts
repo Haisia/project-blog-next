f484169650eaf79219dccaef1b5b94f9a661f7b0
최준혁 <cwnsgur13579@5finger.co.kr>
2025-04-22 18:52:37 +0900

    신규: #15 Project Log 디테일 페이지 구현

--- src/api/fetchProjectLog.ts
+++ src/api/fetchProjectLog.ts
@@ -1,15 +1,45 @@
-import {Project} from "@/types/Project";
-
 const baseUrl = `${process.env.NEXT_PUBLIC_BE_HOST}/api/blog/projectlog`;
 
 export const fetchAllProjectLog = async () => {
-  const fetchResult = await fetch(`${baseUrl}/all`, {next:{revalidate:30}});
+  const fetchResult = await fetch(`${baseUrl}/all`, {next: {revalidate: 30}});
   const {projects}: { projects: Project[] } = await fetchResult.json();
 
   return projects;
 }
 
-export const fetchProjectLog = async (id:string) => {
+export const fetchProjectLog = async (id: string) => {
   const projects = await fetchAllProjectLog();
   return projects.filter((project) => project.id === parseInt(id));
+}
+
+export const fetchProjectLogPost = async (id: string) => {
+  const fetchResult = await fetch(`${baseUrl}/${id}`, {next: {revalidate: 30}});
+  const {postId, postTitle, postContent, createdAt, updatedAt} = await fetchResult.json();
+
+  return {
+    id: postId,
+    title: postTitle,
+    content: postContent,
+    createdAt, updatedAt
+  };
+}
+
+export interface Project {
+  id: number;
+  title: string;
+  categories?: ProjectCategory[];
+}
+
+export interface ProjectCategory {
+  id: number;
+  title: string;
+  posts?: ProjectPost[];
+}
+
+export interface ProjectPost {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
 }
--- src/app/projectlog/[id]/page.tsx
+++ src/app/projectlog/[id]/page.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {fetchProjectLogPost} from "@/api/fetchProjectLog";
+import MarkdownPost from "@/components/markdownPost";
 
-const Page = () => {
+const group = "Project Log"
+const groupLink = "/projectlog"
+
+const Page = async (
+  { params }: Readonly<{ params: Promise<{ id: number }> }>
+) => {
+  const { id } = await params
+  const post = await fetchProjectLogPost(id.toString())
+
+  const breadcrumbItems = [
+    {content: group, link: groupLink},
+    {content: post.title, link: `${groupLink}/${id}`}
+  ]
+
   return (
-    <>
-
-    </>
+    <MarkdownPost
+      breadcrumbItems={breadcrumbItems}
+      item={post}
+    />
   );
 };
 
