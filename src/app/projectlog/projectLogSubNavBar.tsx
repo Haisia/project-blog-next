9bf5f764e5472fe9498b79afca0f2436ad59ee12
최준혁 <cwnsgur13579@5finger.co.kr>
2025-04-23 18:02:41 +0900

    기타: 컴포넌트 이름 수정

--- src/app/projectlog/projectLogSubNavBar.tsx
+++ src/app/projectlog/projectLogSubNavBar.tsx
@@ -1,9 +1,9 @@
 import { fetchAllProjectLog } from "@/api/fetchProjectLog";
 import ProjectLogSubNavBarClient from "@/app/projectlog/(clients)/projectLogSubNavBarClient";
 
-const ProjectLogSubNavBarWrapper = async () => {
+const ProjectLogSubNavBar = async () => {
   const projects = await fetchAllProjectLog();
   return <ProjectLogSubNavBarClient projects={projects} />;
 };
 
-export default ProjectLogSubNavBarWrapper;
+export default ProjectLogSubNavBar;
