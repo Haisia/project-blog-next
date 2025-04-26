6dac70176ea00567cea5fd31b8873dce4f47b6f8
Haisia <cwnsgur13579@gmail.com>
2025-04-27 00:03:10 +0900

    신규: #33 Dev Tool 기본 페이지 구현

--- src/app/devnews/page.tsx
+++ src/app/devnews/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {BreadcrumbItem} from "@/components/breadcrumb";
+import MarkdownPost from "@/components/markdownPost";
+import {devNewsDefaultContent} from "@/data/devNewsData";
+
+const pageName = "Dev News"
+const pageLink = "/devnews"
 
 const Page = () => {
+  const breadcrumbItems:BreadcrumbItem[] = [
+    {content:pageName, link: pageLink},
+  ];
+
   return (
     <>
-      <div>pagetest</div>
+      {<MarkdownPost breadcrumbItems={breadcrumbItems} item={devNewsDefaultContent}/>}
     </>
   );
 }
