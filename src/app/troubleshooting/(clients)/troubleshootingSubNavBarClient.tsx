2825486e734381a1d02489d49b17dac2fc665c58
Haisia <cwnsgur13579@gmail.com>
2025-04-27 10:02:11 +0900

    수정: 스타일수정

--- src/app/troubleshooting/(clients)/troubleshootingSubNavBarClient.tsx
+++ src/app/troubleshooting/(clients)/troubleshootingSubNavBarClient.tsx
@@ -2,9 +2,7 @@
 
 import {useMemo} from "react";
 import SubNavBarWithSubTitle from "@/components/subNavBarWithSubTitle";
-import Link from "next/link";
 import useSubNavBarWithSubTitle from "@/hooks/useSubNavBarWithSubTitle";
-import {ProjectLog, ProjectLogDto} from "@/types/projectlog/ProjectLog";
 import {Troubleshooting, TroubleshootingDto} from "@/types/troubleshooting/Troubleshooting";
 
 const baseUrl = "/troubleshooting";
@@ -14,29 +12,15 @@ const TroubleshootingSubNavBarClient =
   const troubleshootingInstances
     = useMemo(() => troubleshootings.map(Troubleshooting.fromObject), [troubleshootings]);
 
-  const {selectedId,setSelectedId, dropDownItems, contentItems } = useSubNavBarWithSubTitle(troubleshootingInstances, baseUrl);
+  const {setSelectedId, dropDownItems, contentItems } = useSubNavBarWithSubTitle(troubleshootingInstances, baseUrl);
 
   return (
     <SubNavBarWithSubTitle
-      preLi={<PreLi href={`/projectlog?projectId=${selectedId}`}/>}
       selectedIdSetter={setSelectedId}
       contentsItems={contentItems}
       dropDownItems={dropDownItems}
     />
   );
 };
-
-const PreLi = ({href}:{href:string}) => {
-  return (
-    <>
-      <li className={"marker:size-[1.25rem] text-[1rem]"}>
-        <Link className={`hover:text-mypurple-100 hover:bg-neutral-700/90 p-2 rounded-lg hover:border-mypurple-100 border border-transparent flex  justify-between`} 
-              href={href}>
-          프로젝트 개요
-        </Link>
-      </li>
-    </>
-  )
-}
 
 export default TroubleshootingSubNavBarClient;
