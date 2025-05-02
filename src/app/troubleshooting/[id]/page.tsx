e4e49e3f5dff3483bf0d02e5d308d4389ebf20fe
Haisia <cwnsgur13579@gmail.com>
2025-05-02 15:33:45 +0900

    신규: 트러블슈팅 단건조회 페이지

---
+++ src/app/troubleshooting/[id]/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import MarkdownPost from "@/components/markdownPost";
+import {staticParamsGenerator} from "@/utils/staticParamsGenerator";
+import {notFound} from "next/navigation";
+import {fetchTroubleshootingPost} from "@/api/fetchTroubleshooting";
+
+const pageName = "Troubleshooting"
+const pageLink = "/troubleshooting"
+
+export function generateStaticParams() {
+  return staticParamsGenerator(50);
+}
+
+const Page = async (
+  { params }: Readonly<{ params: Promise<{ id: number }> }>
+) => {
+  const { id } = await params
+  const post = await fetchTroubleshootingPost(id.toString())
+
+  if (!post) {
+    notFound();
+  }
+
+  const breadcrumbItems = [
+    {content: pageName, link: pageLink},
+    {content: post.title, link: `${pageLink}/${id}`}
+  ]
+
+  return (
+    <MarkdownPost
+      breadcrumbItems={breadcrumbItems}
+      item={post}
+    />
+  );
+};
+
+export default Page;
